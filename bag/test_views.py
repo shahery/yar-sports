5646823f816129cf5ab7cb179fbcdb4af6d9847c
shahery <shaheryar924@gmail.com>
2022-09-03 22:24:08 +0000

    Add coverage report and two more tests

--- bag/test_views.py
+++ bag/test_views.py
@@ -13,3 +13,12 @@ class TestBagViews(TestCase):
         response = self.client.get('/bag/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
+
+    def test_can_add_to_bag(self):
+        response = self.client.post('add/<item_id>/',
+                                    {'title': 'Test add to bag'})
+        self.assertNotEqual(response.status_code, 301)
+
+    def test_can_remove_from_bag(self):
+        response = self.client.get('remove/<item_id>/')
+        self.assertNotEqual(response.status_code, 301)
