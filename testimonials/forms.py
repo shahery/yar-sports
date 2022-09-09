0df45c74df67205cf177cb2fe689a65283cd959c
shahery <shaheryar924@gmail.com>
2022-09-09 21:09:15 +0000

    Add changes in the author field of testimonial model

--- testimonials/forms.py
+++ testimonials/forms.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
+from django import forms
 from django.forms import ModelForm
 from .models import Testimonial
 
@@ -9,3 +10,9 @@ class TestimonialForm(ModelForm):
     class Meta:
         model = Testimonial
         fields = ('author', 'text', 'active',)
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'form-control',
+                                             'value': '', 'id': 'elder',
+                                             'type': 'hidden'}),
+        }
--- testimonials/models.py
+++ testimonials/models.py
@@ -3,12 +3,13 @@
 # pylint: disable=missing-function-docstring
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Testimonial(models.Model):
 
     text = models.TextField()
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     affiliation = models.CharField(max_length=100, blank=True)
     added = models.DateTimeField(default=timezone.now)
     active = models.BooleanField(default=False)
