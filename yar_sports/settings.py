1263ae86c78c1413a61f5cd72043015b58645665
shahery <shaheryar924@gmail.com>
2022-09-16 10:43:16 +0000

    Remove secret key and set debug

--- yar_sports/settings.py
+++ yar_sports/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&wpm=#*7b&0g*t9+&*c=_buoy*dhca1u56p1r1786w*1fs1es'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['yar-sports.herokuapp.com', 'localhost']
 
