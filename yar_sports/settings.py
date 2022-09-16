15fd532abb3ab08b964b2771a6687bb4553dda0a
shahery <shaheryar924@gmail.com>
2022-09-16 14:30:22 +0000

    Add correction in the region name

--- yar_sports/settings.py
+++ yar_sports/settings.py
@@ -189,7 +189,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'yar-sports'
-    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
