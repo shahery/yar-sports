9e14737853107d00102422b14433eeab06f5cdda
shahery <shaheryar924@gmail.com>
2022-09-18 09:51:11 +0000

    Add changes to improve accessibilty

--- yar_sports/settings.py
+++ yar_sports/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['yar-sports.herokuapp.com', 'localhost']
 
